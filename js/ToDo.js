9876026366149daa66cb34993cc3ed59f9232b63
RWAMing <art.rwam@gmail.com>
2023-12-12 17:26:07 +0900

    231212 edti - drag To Do(no order change)

--- js/ToDo.js
+++ js/ToDo.js
@@ -62,26 +62,61 @@ window.addEventListener('load',function(){
             function open_edit(){
                 mode_edit = true;
                 show_btn_change_order();
+                drag_btn_change_order();
             }
             // list-style 모양 변경
-            function show_btn_change_order() {
-                var lists_style = document.querySelectorAll('.list_style');
-                lists_style.forEach(function(list_style) {
-                    list_style.style.opacity='0';
-                    list_style.style.backgroundColor='transparent';
-                    var appear_timer;
-                    appear_timer = setTimeout(function() { // 이동 모드로 변경
-                        list_style.innerHTML = '&equiv;';
-                        list_style.style.position = 'relative';
-                        list_style.style.right = '7px';
-                        list_style.style.height = '40px';
-                        list_style.style.margin = '0';
-                        list_style.style.opacity='1';
-                        list_style.style.cursor='pointer';
-                        list_style.style.userSelect='none';
-                    }, 300);
-                });
-            }
+                function show_btn_change_order() {
+                    var lists_style = document.querySelectorAll('.list_style');
+                    lists_style.forEach(function(list_style) {
+                        list_style.style.opacity='0';
+                        list_style.style.backgroundColor='transparent';
+                        var appear_timer;
+                        appear_timer = setTimeout(function() { // 이동 모드로 변경
+                            list_style.innerHTML = '&equiv;';
+                            list_style.style.position = 'relative';
+                            list_style.style.right = '7px';
+                            list_style.style.height = '40px';
+                            list_style.style.margin = '0';
+                            list_style.style.opacity='1';
+                            list_style.style.cursor='pointer';
+                            list_style.style.userSelect='none';
+                        }, 300);
+                    });
+                }
+            // list-style 드래그
+                var lists_style_edit;
+                function drag_btn_change_order() {
+                    lists_style_edit = document.querySelectorAll('.list_wrap ul p');
+                    lists_style_edit.forEach(drag_ready); // 모든 To Do List 드래그 준비
+                }
+            // 드래그 이벤트리스너 준비
+                function drag_ready(element_drag){
+                    element_drag.addEventListener('mousedown',drag_start);
+                    window.addEventListener('mouseup',drag_finish);
+                    var clicked_Y;
+                    var drag_li = element_drag.closest('li');
+                // 드래그 준비
+                    function drag_start(element_drag){
+                        clicked_Y = element_drag.clientY;
+                        window.addEventListener('mousemove',drag_move);
+                    }
+                // 드래그 중
+                    function drag_move (view){
+                        var dragged_Y = view.clientY;
+                        var dragged_distance = dragged_Y-clicked_Y;
+                        var ul_top_Y = document.querySelector('ul').getBoundingClientRect().top;
+                        var ul_bottom_Y = document.querySelector('ul').getBoundingClientRect().bottom;
+                        if(dragged_Y>=ul_top_Y+30&&dragged_Y<=ul_bottom_Y-15){ // ul 뚫지 못하게
+                            drag_li.style.top = dragged_distance+'px';
+                        }
+                    }
+                // 드래그 끝
+                    function drag_finish(){
+                        window.removeEventListener('mousemove',drag_move);
+                        drag_li.style.top ='0px';
+                    }
+                }
+
         // btn_edit 인식 (ToDo 없어서 삭제될 때 인식 필요)
             function btn_edit(){
                 return document.querySelector('.btn_edit');
@@ -102,6 +137,18 @@ window.addEventListener('load',function(){
         function add_listStyle() { // list style 생성
             var new_listStyle = document.createElement('p');
             new_listStyle.classList.add('list_style');
+            if(mode_edit){ // edit 모드일 경우
+                new_listStyle.innerHTML = '&equiv;';
+                new_listStyle.style.opacity='0';
+                new_listStyle.style.backgroundColor='transparent';
+                new_listStyle.style.position = 'relative';
+                new_listStyle.style.right = '7px';
+                new_listStyle.style.height = '40px';
+                new_listStyle.style.margin = '0';
+                new_listStyle.style.opacity='1';
+                new_listStyle.style.cursor='pointer';
+                new_listStyle.style.userSelect='none';
+            }
             return new_listStyle
         }
         function add_btn_remove() { // ToDo 지우기 버튼 x 만들기
@@ -121,20 +168,25 @@ window.addEventListener('load',function(){
             return new_btn_remove;
         }
 
-    // 유저입력 To Do, placeholder To Do 입력
-        btn_add.addEventListener('click',add_ToDo); // 클릭 -> To Do 추가 실행
-        function add_ToDo() { // To Do 추가
-            if (have_TD_guide()){ // 설명 있을때는,
-                TD_guide.remove(); // 설명 삭제 
-                listBox.append(new_ul); // new ul
-                if (document.querySelectorAll('.btn_edit').length==0) { // edit 없을때만 생성
-                    list_ul().append(add_btn_edit());
+        // 추가된 To Do 내용 - 유저입력내용 || Placeholder내용
+            btn_add.addEventListener('click',add_ToDo); // 클릭 -> To Do 추가 실행
+            function add_ToDo() { // To Do 추가
+                if (have_TD_guide()){ // 설명 있을때는,
+                    TD_guide.remove(); // 설명 삭제 
+                    listBox.append(new_ul); // new ul
+                    if (document.querySelectorAll('.btn_edit').length==0) { // edit 없을때만 생성
+                        list_ul().append(add_btn_edit());
+                    }
                 }
-            }
                 list_ul().append(add_TD()); // new li
                 typeBox_TD.value = ''; // 기존 입력내용 삭제
                 typeBox_TD.placeholder = random_TD_PH_ex(); // placeholder변경
-        }
+                if(mode_edit){
+                    var new_edit = document.querySelector('.list_wrap ul li:last-of-type p');
+                    console.log(new_edit);
+                    new_edit.addEventListener('mousedown',drag_ready(new_edit));
+                }
+            }
 
 
 // 예시 placeholer - show, hide by focus
